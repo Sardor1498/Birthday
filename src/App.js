0ba71765565a87018da83c141489fe50a39e8e69
Sardor1498 <tsardor1498@gmail.com>
2021-11-27 12:31:13 +0500

    sa

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import Birthday from './components/Birthday';
 
 
 function App() {
 
-  const [second, setSecond] = useState(0)
-  const [minute, setMinute] = useState(0)
-  const [hour, setHour] = useState(0)
+  // const [second, setSecond] = useState(0)
+  // const [minute, setMinute] = useState(0)
+  // const [hour, setHour] = useState(0)
 
-  // let interval = useRef();
-  const setClock = () => {
-    setInterval(() => {
-      const currentDate = new Date();
-      let secondRatio = currentDate.getSeconds() / 60;
-      let minuteRatio = (secondRatio + currentDate.getMinutes()) / 60;
-      let hourRatio = (minuteRatio + currentDate.getHours()) / 12;
-
-
-      setSecond(secondRatio);
-      setMinute(minuteRatio);
-      setHour(hourRatio)
-
-      setSecond({ secondRatio: secondRatio = currentDate.getSeconds() / 60 })
-      setMinute({ minuteRatio: minuteRatio = (secondRatio + currentDate.getMinutes()) / 60 })
-      setHour({ hour: hourRatio = (minuteRatio + currentDate.getHours()) / 12 });
-    }, 1000)
-  }
+  // // let interval = useRef();
+  // const setClock = () => {
+  //   setInterval(() => {
+  //     const currentDate = new Date();
+  //     let secondRatio = currentDate.getSeconds() / 60;
+  //     let minuteRatio = (secondRatio + currentDate.getMinutes()) / 60;
+  //     let hourRatio = (minuteRatio + currentDate.getHours()) / 12;
 
 
-  useEffect(() => {
-    setClock()
-    return () => {
-      clearInterval()
-    }
-  }, [])
+  //     setSecond(secondRatio);
+  //     setMinute(minuteRatio);
+  //     setHour(hourRatio)
+
+  //     setSecond({ secondRatio: secondRatio = currentDate.getSeconds() / 60 })
+  //     setMinute({ minuteRatio: minuteRatio = (secondRatio + currentDate.getMinutes()) / 60 })
+  //     setHour({ hour: hourRatio = (minuteRatio + currentDate.getHours()) / 12 });
+  //   }, 1000)
+  // }
+
+
+  // useEffect(() => {
+  //   setClock()
+  //   return () => {
+  //     clearInterval()
+  //   }
+  // }, [])
 
 
   return (
